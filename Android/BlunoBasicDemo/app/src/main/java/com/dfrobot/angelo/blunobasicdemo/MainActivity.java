71a0f18b76b646fd58b62cb2c2baa90e2dbabbfe
SeokJun Hong <hong921122@gmail.com>
2016-01-13 16:46:09 +0900

    write a log when ble event triggered

--- Android/BlunoBasicDemo/app/src/main/java/com/dfrobot/angelo/blunobasicdemo/MainActivity.java
+++ Android/BlunoBasicDemo/app/src/main/java/com/dfrobot/angelo/blunobasicdemo/MainActivity.java
@@ -3,15 +3,32 @@ package com.dfrobot.angelo.blunobasicdemo;
 import android.content.Context;
 import android.os.Bundle;
 import android.content.Intent;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity  extends BlunoLibrary {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Calendar;
+
+public class MainActivity  extends BlunoLibrary implements OnClickListener {
+	private final int HIGH = 1;
+	private final int LOW = 0;
+	private final String filePath = Environment.getExternalStorageDirectory().getPath() + "/log.txt" ;
+
+	private static int subject_id = 0;
+	private int previousState = HIGH;
+
 	private Button buttonScan;
 	private Button buttonSerialSend;
+	private Button buttonAddSubject;
 	private EditText serialSendText;
 	private TextView serialReceivedText;
 	
@@ -28,26 +45,27 @@ public class MainActivity  extends BlunoLibrary {
         serialSendText=(EditText) findViewById(R.id.serialSendText);			//initial the EditText of the sending data
 
         buttonSerialSend = (Button) findViewById(R.id.buttonSerialSend);		//initial the button for sending the data
-        buttonSerialSend.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
-
-				serialSend(serialSendText.getText().toString());				//send the data to the BLUNO
-			}
-		});
-
+		buttonSerialSend.setOnClickListener(this);
         buttonScan = (Button) findViewById(R.id.buttonScan);					//initial the button for scanning the BLE device
-        buttonScan.setOnClickListener(new OnClickListener() {
+		buttonScan.setOnClickListener(this);
+		buttonAddSubject = (Button) findViewById(R.id.subject_delim);
+		buttonAddSubject.setOnClickListener(this);
+	}
 
-			@Override
-			public void onClick(View v) {
-				// TODO Auto-generated method stub
 
-				buttonScanOnClickProcess();										//Alert Dialog for selecting the BLE device
-			}
-		});
+	@Override
+	public void onClick(View v) {
+		switch(v.getId()) {
+			case R.id.buttonSerialSend:
+				serialSend(serialSendText.getText().toString());
+				break;
+			case R.id.buttonScan:
+				buttonScanOnClickProcess();
+				break;
+			case R.id.subject_delim:
+				writeSubjectDelim();
+				break;
+		}
 	}
 
 	protected void onResume(){
@@ -57,7 +75,7 @@ public class MainActivity  extends BlunoLibrary {
 	}
 	
 	
-	
+
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		onActivityResultProcess(requestCode, resultCode, data);					//onActivityResult Process by BlunoLibrary
@@ -109,7 +127,54 @@ public class MainActivity  extends BlunoLibrary {
 		// TODO Auto-generated method stub
 		serialReceivedText.append(theString);							//append the text into the EditText
 		//The Serial data from the BLUNO may be sub-packaged, so using a buffer to hold the String is a good choice.
-					
+		try {
+
+			int state = Integer.parseInt(theString.trim());
+
+			if (state != previousState) {
+				if(previousState == LOW) {
+					Calendar c = Calendar.getInstance();
+					writeToFile(String.format("%d-%d-%d:%d-%d-%d, %d\n",
+							c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH), c.get(Calendar.HOUR), c.get(Calendar.MINUTE), c.get(Calendar.SECOND), subject_id));
+				}
+				previousState = state;
+			}
+		} catch(Exception e) {
+			Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+		}
 	}
 
+	private void writeToFile(String data) {
+		try {
+			File directory = new File(Environment.getExternalStorageDirectory(), "bluetooth_log");
+			if (!directory.exists()) {
+				directory.mkdirs();
+			}
+
+			File log_file = new File(directory, "log.txt");
+			FileOutputStream fOut = null;
+			if(!log_file.exists()) {
+				fOut = new FileOutputStream(log_file);
+			} else {
+				fOut = new FileOutputStream(log_file, true);
+			}
+
+			OutputStreamWriter outputStreamWriter = new OutputStreamWriter(fOut);
+			outputStreamWriter.write(data);
+			outputStreamWriter.close();
+			Toast.makeText(getApplicationContext(), data, Toast.LENGTH_LONG).show();
+		}
+		catch (IOException e) {
+			Toast.makeText(getApplicationContext(), "fail", Toast.LENGTH_LONG).show();
+			Log.e("Exception", "File write failed: " + e.toString());
+		}
+	}
+
+	private void writeSubjectDelim() {
+		subject_id ++;
+		Calendar c = Calendar.getInstance();
+		String delim = String.format("=====================subject:%d;datetime:%d-%d-%d;%d-%d-%d;=====================\n",
+				subject_id, c.get(Calendar.YEAR), c.get(Calendar.MONTH), c.get(Calendar.DAY_OF_MONTH), c.get(Calendar.HOUR), c.get(Calendar.MINUTE), c.get(Calendar.SECOND));
+		writeToFile(delim);
+	}
 }
